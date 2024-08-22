cfe80dcdc02f5bf31bf71122b3257fdfd9fe16bb
sandhya-1611 <141938427+sandhya-1611@users.noreply.github.com>
2024-08-22 20:33:32 +0530

    created the navbar component

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function MenuItem({ title, address, Icon }) {
   return (
-    <Link href={address} className='hover:text-amber-500'>
+    <Link href={address} className='hover:text-purple-400'>
       <Icon className="text-2xl sm:hidden"/>
       <p className='uppercase hidden sm:inline text-sm'>{title}</p>
     </Link>
---
+++ src/components/NavbarItem.jsx
@@ -0,0 +1,23 @@
+'use client';
+
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
+
+export default function NavbarItem({ title, param }) {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get('genre');
+  return (
+    <div>
+      <Link
+        className={`hover:text-purple-500 font-semibold ${
+          genre === param
+            ? 'underline underline-offset-8 decoration-4 decoration-purple-500 rounded-lg'
+            : ''
+        }`}
+        href={`/?genre=${param}`}
+      >
+        {title}
+      </Link>
+    </div>
+  );
+}
