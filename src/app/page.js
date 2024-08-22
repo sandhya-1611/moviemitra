ea99052ccf955b8a7bdfc0d899d682491c9e3ac6
sandhya-1611 <141938427+sandhya-1611@users.noreply.github.com>
2024-08-22 21:31:04 +0530

    handled the error and added loading effect

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,8 @@ export default async function Home({ searchParams }) {
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=en-US&page=1`
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
   );
   
   const data = await res.json();
@@ -16,7 +17,7 @@ export default async function Home({ searchParams }) {
     throw new Error('Failed to fetch data');
   }
   const results = data.results;
-  console.log(results);
+  // console.log(results);
   
   return (
     <div>
