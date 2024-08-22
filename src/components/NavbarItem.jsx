ccd2b612f8fd9ec31f37a77b3d650450d99f72b7
sandhya-1611 <141938427+sandhya-1611@users.noreply.github.com>
2024-08-22 23:33:40 +0530

    minor fix2

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -5,7 +5,7 @@ import { useSearchParams } from 'next/navigation';
 
 export default function NavbarItem({ title, param }) {
   const searchParams = useSearchParams();
-  const genre = searchParams.get('genre');
+  const genre = searchParams ? searchParams.get('genre') : null;
   return (
     <div>
       <Link
