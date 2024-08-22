18d5272322fea4ffcac7afac6802b420b48e3e71
sandhya-1611 <141938427+sandhya-1611@users.noreply.github.com>
2024-08-23 00:52:28 +0530

    minor fix 3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,24 @@
+// import NavbarItem from './NavbarItem';
+
+// export default function Navbar() {
+//   return (
+//     <div className='flex dark:bg-gray-600 bg-purple-100 p-4 lg:text-lg justify-center gap-6'>
+//       <NavbarItem title='Trending' param='fetchTrending' />
+//       <NavbarItem title='Top Rated' param='fetchTopRated' />
+//     </div>
+//   );
+// }
+
+import React, { Suspense } from 'react';
 import NavbarItem from './NavbarItem';
 
 export default function Navbar() {
   return (
     <div className='flex dark:bg-gray-600 bg-purple-100 p-4 lg:text-lg justify-center gap-6'>
-      <NavbarItem title='Trending' param='fetchTrending' />
-      <NavbarItem title='Top Rated' param='fetchTopRated' />
+      <Suspense fallback={<div>Loading...</div>}>
+        <NavbarItem title='Trending' param='fetchTrending' />
+        <NavbarItem title='Top Rated' param='fetchTopRated' />
+      </Suspense>
     </div>
   );
 }
